438683e94391542539d7c2380ce579ffe873b97b
Tijana0 <94015593+Tijana0@users.noreply.github.com>
2026-05-24 16:58:06 +0200

    init empty base project

---
+++ app.js
@@ -0,0 +1,35 @@
+d3.csv('OpenExoplanetCatalogue.csv').then(data => {
+  console.log("Data loaded:", data);
+  
+  const lensSvg = d3.select('#lens-svg');
+  const width = 460;
+  const height = 460;
+  
+  lensSvg.append("text")
+    .attr("x", width/2)
+    .attr("y", height/2)
+    .attr("text-anchor", "middle")
+    .attr("fill", "var(--ink-3)")
+    .text("D3 Viewport");
+
+  const scroller = scrollama();
+
+  function handleStepEnter(response) {
+    console.log("Step entered:", response.index);
+    // response.element, response.direction, response.index
+    const el = d3.select(response.element);
+    
+    d3.select('#n-tag').text(`Scene 0${response.index + 1}`);
+    d3.select('#n-text').text(`You are currently on step ${response.index + 1}`);
+  }
+
+  scroller
+    .setup({
+      step: '.step',
+      offset: 0.5,
+      debug: false
+    })
+    .onStepEnter(handleStepEnter);
+
+  window.addEventListener('resize', scroller.resize);
+});
